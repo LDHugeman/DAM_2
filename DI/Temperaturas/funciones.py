b4b682247bc3cb44e6517090250a58cc18767f9c
luisd <luisdavid.vp@gmail.com>
2019-10-06 22:10:00 +0200

    Primera version temperaturas

--- DI/Temperaturas/funciones.py
+++ DI/Temperaturas/funciones.py
@@ -1,6 +1,6 @@
 def celsius_to_fahrenheit(grados_celsius):
-    resultado = 1.4 * float(grados_celsius) + 32.0  # resultado = 1.4 * float(grados_celsius) + 32.0
-    return resultado
+    grados_celsius = float(grados_celsius)
+    return (grados_celsius * 9 / 5) + 32  # resultado = 1.4 * float(grados_celsius) + 32.0
 
 
 def celsius_to_kelvin(grados_celsius):
