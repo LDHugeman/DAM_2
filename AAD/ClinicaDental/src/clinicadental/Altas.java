3c17f94dd4662b752f15ab65f8f20a0d952c7f51
Betnu <50693620+Betnu@users.noreply.github.com>
2019-12-05 14:05:36 +0100

    Add files via upload

---
+++ AAD/ClinicaDental/src/clinicadental/Altas.java
@@ -0,0 +1,41 @@
+package clinicadental;
+import objetos.*;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+/**
+ *
+ * @author a16albertolg
+ */
+public class Altas {
+    public static void nuevoLimpiador(Limpiador limpiador){
+        guardar(limpiador);
+    }
+    public static void nuevoDentista(Dentista dentista){
+        guardar(dentista);
+    }
+    public static void nuevaConsulta(Consulta consulta){
+        guardar(consulta);
+    }
+    public static void nuevoPaciente(Paciente paciente){
+        guardar(paciente);
+    }
+    public static void nuevoHistorial(Historial historial){
+        guardar(historial);
+    }
+    public static void nuevaCita(Cita cita){
+        guardar(cita);
+    }
+    public static void guardar(Object objeto){
+        Session session;
+        try{
+            session = NewHibernateUtil.getSession();
+            session.beginTransaction();
+            session.save(objeto);
+            session.getTransaction().commit();
+            session.close();
+        }catch(HibernateException excepcion){
+            System.err.printf("Error al guardar\n");
+            System.out.println(excepcion.getMessage());
+        }
+    }
+}
