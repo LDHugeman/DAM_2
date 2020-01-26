dd2471f6766748a3cdf3b463636267ddad10b1b6
a18luisdvp <a18luisdvp@EQUIP077238.xunta.local>
2020-01-26 15:51:00 +0100

    ejercicio completo

--- PSP/2ª Evaluación/Ejemplos/TestHilos1/src/testhilos1/TestHilos1.java
+++ PSP/2ª Evaluación/Ejemplos/TestHilos1/src/testhilos1/TestHilos1.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package testhilos1;
 
 /**
@@ -10,17 +6,9 @@ package testhilos1;
  */
 public class TestHilos1 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        ContadorAdelante cuentaAdelante1=new ContadorAdelante("contador++");
-        
+        ContadorAdelante cuentaAdelante1 = new ContadorAdelante("contador++");
         System.out.println("");
-        /*    
-        que ponemos aquï?.
-        tal y como está los dos hilos se ejecutan asincronicamnete....
-        */
         System.out.println("Finalización del hilo, ahora fin hilo primario");
     }
 }
