0a13d561efb943dd97e59925e60e5545604daeb4
a18luisdvp <a18luisdvp@EQUIP077238.xunta.local>
2020-01-26 15:51:07 +0100

    ejercicio completo

--- PSP/2ª Evaluación/Ejemplos/TestDemonio/src/testdemonio/CDemonio.java
+++ PSP/2ª Evaluación/Ejemplos/TestDemonio/src/testdemonio/CDemonio.java
@@ -11,16 +11,16 @@ public class CDemonio extends Thread {
         setName("demoniobip");
         start();
     }
-
+    
     public void run() {
         char sonido = '\u0007';
         while (true) {
             try {
                 System.out.print("aaaaa");
-                sleep(1000); //cantidad de milisegundos
+                sleep(1000); 
             } catch (InterruptedException e) {
             }
             System.out.print(sonido);
+          	}
         }
     }
-}
